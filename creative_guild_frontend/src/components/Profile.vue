f9ccfd7e317b2153bec587167d5ccc22a59b93b2
Manali Patel <113155434+Manali1321@users.noreply.github.com>
2024-02-07 16:13:10 -0500

    similar css for profile as mockup

--- creative_guild_frontend/src/components/Profile.vue
+++ creative_guild_frontend/src/components/Profile.vue
@@ -6,34 +6,157 @@
           <img :src="user.profile_picture" alt="Profile Picture" />
         </div>
         <div class="profile-details">
-          <h2>{{ user.name }}</h2>
-          <p>{{ user.email }}</p>
-          <p>{{ user.phone }}</p>
-          <p>{{ user.bio }}</p>
+          <h2 class="primary-font">{{ user.name }}</h2>
+          <div class="bio">
+            <div>
+              <div class="profile-details-title">Bio</div>
+              <p class="secondary-font">{{ user.bio }}</p>
+            </div>
+            <div>
+              <div class="profile-details-title">Phone</div>
+              <p class="secondary-font highlight-font">{{ user.phone }}</p>
+              <div class="profile-details-title">Email</div>
+              <p class="secondary-font highlight-font">{{ user.email }}</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
     <div v-else>
       <p>Loading user profile...</p>
     </div>
-    <h1>User Albums</h1>
-    <div class="albums" v-if="albums.length">
-      <div class="album" v-for="album in albums" :key="album.id">
-        <div class="album-image">
-          <img :src="album.img" alt="Album Image" />
-        </div>
-        <div class="album-details">
-          <h2>{{ album.title }}</h2>
-          <p>{{ album.description }}</p>
-          <p>{{ album.date }}</p>
+    <div class="albums">
+      <div class="album-container" v-if="albums.length">
+        <div class="album" v-for="album in albums" :key="album.id">
+          <div class="album-image">
+            <img :src="album.img" alt="Album Image" />
+            <p class="album-name">{{ album.title }}</p>
+          </div>
+          <div class="album-details">
+            <p class="album-description">{{ album.description }}</p>
+            <div class="album-footer">
+              <span v-if="album.featured" class="featured-icon">❤️</span>
+              <span class="album-date">{{ album.date }}</span>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-    <div v-else>
-      <p>No albums found.</p>
+      <div v-else>
+        <p>No albums found.</p>
+      </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.profile-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 70%; /* Adjust the width as needed */
+
+}
+.album-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+.profile {
+  background-color: #f9f9f9;
+  border-radius: 10px;
+  padding: 20px;
+  margin-bottom: 20px;
+}
+
+.profile-info, .albums {
+  display: flex;
+  align-items: center;
+  width: 100%;
+}
+
+.profile-info {
+  align-items: flex-start;
+}
+
+.profile-picture img {
+  border-radius: 50%;
+  width: 100px;
+  height: 100px;
+  margin-right: 20px;
+}
+
+.profile-details {
+  flex-grow: 1;
+}
+
+.profile-details h2 {
+  font-weight: 500;
+}
+
+.profile-details .bio {
+  display: flex;
+}
+.highlight-font{
+  color: rgb(244, 63, 93);
+}
+.profile-details-title{
+  font-size: 14px;
+  font-weight: 300;
+}
+.profile-details p {
+  margin-bottom: 5px;
+}
+
+.album {
+  width: calc(33% - 10px);
+  margin-bottom: 20px;
+  background-color: #f9f9f9;
+  border-radius: 10px;
+}
+
+.album img {
+  width: 100%;
+  height: 200px;
+  border-radius: 10px 10px 0 0;
+}
+
+.album-image {
+  position: relative;
+}
+
+.album-name {
+  position: absolute;
+  bottom: 5px;
+  margin: 0px;
+  color: white;
+  font-size: 20px;
+  font-weight: bold;
+  padding: 10px;
+}
+
+.album-details {
+  padding: 10px;
+}
+
+.album-description {
+  margin-bottom: 10px;
+}
+
+.album-footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.featured-icon {
+  color: red;
+}
+
+.album-date {
+  font-size: 0.8rem;
+}
+</style>
+
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
@@ -45,6 +168,7 @@ interface Album {
   description: string
   img: string
   date: string
+  featured: boolean
 }
 
 interface User {
@@ -75,66 +199,3 @@ onMounted(() => {
     })
 })
 </script>
-
-<style scoped>
-.profile-container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.profile {
-  background-color: #f9f9f9;
-  border-radius: 10px;
-  padding: 20px;
-  margin-bottom: 20px;
-}
-
-.profile-info {
-  display: flex;
-  align-items: center;
-}
-
-.profile-picture img {
-  border-radius: 50%;
-  width: 100px;
-  height: 100px;
-  margin-right: 20px;
-}
-
-.profile-details h2 {
-  font-size: 24px;
-  margin-bottom: 10px;
-}
-
-.profile-details p {
-  margin-bottom: 5px;
-}
-
-.albums {
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-  gap: 20px;
-}
-
-.album {
-  background-color: #f9f9f9;
-  border-radius: 10px;
-  padding: 20px;
-}
-
-.album img {
-  width: 100%;
-  height: auto;
-  border-radius: 10px;
-}
-
-.album-details h2 {
-  font-size: 20px;
-  margin-bottom: 10px;
-}
-
-.album-details p {
-  margin-bottom: 5px;
-}
-</style>
